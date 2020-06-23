ec53e0a216c88f24d8ef3086731461d41b53a7cf
Angel <45075049+Awesome-Angel@users.noreply.github.com>
2020-06-22 18:09:13 -0700

    Added skeleton files for the time calculator

---
+++ TimeCalculator/time_calc.js
@@ -0,0 +1,29 @@
+var tc = {
+
+	// Start state of the calculator
+	start: function () {
+	
+		// Everything should be at 00
+	
+	
+	},
+	
+	// function for User input (hours: minutes: seconds)
+	input: function () {
+	
+	
+		// User presses enter --> Total display updates and user input box resets
+	
+	
+	},
+	
+	// What about a clear time button?
+	
+	reset: function () {
+	
+	}
+	
+};
+
+
+window.addEventListener("load", tc.start);
